09f9aaca0cff7e302b9e0f953fea06460ca88c35
Erika Stiebel <erikastiebel@gmail.com>
2017-10-28 18:01:53 +0200

    added more config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,52 @@ module.exports = {
   entry: {
     main: './main.jsx'
   },
-
   output: {
-    path: path.join(__dirname, './dist/assets'),
+    path: path.resolve(__dirname, './dist/assets'),
     filename: '[name].bundle.js',
     publicPath: "/assets/"
   },
-
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'react-hot-loader', 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+      },
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'react-hot-loader', 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+      },
+    },
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [
+        'url-loader?limit=10000',
+        'img-loader'
+      ]
+    }]
+  },
+  plugins: [
+      new ExtractTextPlugin('style.css')
+  ],
+  resolve: {
+   extensions: ['.js', '.jsx'],
+ },
+ watch: true
 };
