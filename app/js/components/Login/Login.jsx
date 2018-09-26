ed576157254ebf8468557d1503d41b4ccd6063b2
Erika Stiebel <erikastiebel@gmail.com>
2018-09-26 14:54:46 +0200

    Empty form on submition

--- app/js/components/Login/Login.jsx
+++ app/js/components/Login/Login.jsx
@@ -12,39 +12,42 @@ class Login extends Component {
     state: PropTypes.object.isRequired
   }
 
-  state = {
-    email: '',
-    password: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    this.submitMenuListHandler = this.submitMenuListHandler.bind(this);
+
   }
 
-  onEmailChange = (e) => {
-    const email = e.target.value;
-    this.setState({ email });
+  inputChangeHandler = (e) => {
+    const target = e.target;
+    const name = target.name;
+    this.setState({[name]: e.target.value});
   };
-
-  onPasswordChange = (e) => {
-    const password = e.target.value;
-    this.setState({ password });
-  };
-
-
 
   submitMenuListHandler = e => {
-    e.preventDefault();
+    alert(this.state.email + ' & ' + this.state.password);
+    if (e) e.preventDefault();
     this.props.dispatch(loginUserAction(
       this.state.email,
       this.state.password)
-    )
-
+    );
+    this.setState({ email: '', password: ''});
   }
+
 
   render() {
     return (
       <div className={style.login_form__container}>
         <h2>Logga in</h2>
         <form id="login-form" onSubmit={this.submitMenuListHandler} className={style.login_form} name="userLogin" role="form" action="post">
-          <input id="email" className={style.login_form__email} onChange={this.onEmailChange} type="email" name="email" placeholder="E-post" />
-          <input id="password" className={style.login_form__password} onChange={this.onPasswordChange} type="password" name="password" placeholder="Lösenord" />
+          <input id="email" className={style.login_form__email} onChange={this.inputChangeHandler} value={this.state.email} type="email" name="email" placeholder="E-post" />
+          <input id="password" className={style.login_form__password} onChange={this.inputChangeHandler} value={this.state.password} type="password" name="password" placeholder="Lösenord" />
           <input
             id="logIn"
             className={style.login_form__button}
